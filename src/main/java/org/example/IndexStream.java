dd4a0d16d7125020623515dd0d7a983d90223e78
JamalEddineEb <jamal-eddine.el-betioui@etu.unice.fr>
2024-04-05 10:31:30 +0200

    IndexStream

--- src/main/java/org/example/IndexStream.java
+++ src/main/java/org/example/IndexStream.java
@@ -1,13 +1,28 @@
 package org.example;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.*;
 
 public class IndexStream<T> implements BasicStream<T> {
 
+    private final int min;
+    private final int max;
+    private final Function<Integer, Optional<T>> f;
+
+    public IndexStream(Function<Integer,Optional<T>> f,int min, int max) {
+        this.f = f;
+        this.min = min;
+        this.max = max;
+    }
+
     @Override
     public void forEach(Consumer<T> action) {
-
+        AtomicInteger i = new AtomicInteger(min);
+        while (i.get() < max) {
+            f.apply(i.get()).ifPresent(action);
+            i.incrementAndGet();
+        }
     }
 
     @Override
